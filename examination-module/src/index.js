4db358aae5e5983dfab5182f0510ac4e39db59d7
Alexandr <alexandr.rogansky@gmail.com>
2018-12-29 14:20:12 +0200

    Questions structure has been added. Styles are  moved to separate directory

--- examination-module/src/index.js
+++ examination-module/src/index.js
@@ -1,58 +1,118 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style/index.css';
+import './style/default.css';
+import './style/radio.css';
 
 class Examine extends React.Component {
+
+    handleSubmit(i) {
+
+    }
 
     render() {
         return (
             <div className="wrapper">
-                <ExamineTitle />
-                <Questions />
+                <form action="" className="examine">
+                    <ExamineTitle
+                        value={{
+                            mainTitle: 'Экзаменационный модуль',
+                            theme: 'Тема: Астрономия'
+                        }}
+                    />
+                    <Questions />
+                    <Submit
+                        onClick={(i) => this.handleSubmit(i)}
+                    />
+                </form>
             </div>
         );
     }
 }
 
-function ExamineTitle() {
-    const title = 'Экзаменационный модуль';
-    const theme = 'Тема: Астрономия';
-    return (
-        <div className="titles">
-            <h1>{title}</h1>
-            <h2>{theme}</h2>
-        </div>
-    );
+function Submit() {
+    return <button className="exam-submit">Ответить</button>
+}
+
+class ExamineTitle extends React.Component {
+
+    render() {
+        return (
+            <div className="titles">
+                <h1>{this.props.value.mainTitle}</h1>
+                <h2>{this.props.value.theme}</h2>
+            </div>
+        )
+    }
 }
 
 class Questions extends React.Component {
     constructor(props) {
         super(props);
-        this.questions = [1, 2, 3, 4, 5];
     }
-
-    renderQuestions(i) {
-        return (
-            <div className="question">
-                <QuestionTitle />
-                <div className="q-body" value={ this.questions[i] }>
-
-                </div>;
-            </div>
-            )
-    }
-
 
     render() {
         return (
             <div className="questions">
-                <div className="question-1">
-                    {this.renderQuestions(0)}
+                <div className="question">
+                    <QuestionTitle
+                        value={'Вопрос 1'}
+                    />
+                    <QuestionText
+                        value={'Сколько планет в солнечной системе?'}
+                    />
+                    <AnswersRadio
+                        value={[7, 5, 9, 8]}
+                    />
                 </div>
-                <div className="question-2">
-                    {this.renderQuestions(1)}
+                <div className="question">
+                    <QuestionTitle
+                        value={'Вопрос 2'}
+                    />
+                    <QuestionText
+                        value={'Что характерно для кометы?'}
+                    />
+                    <AnswersCheckbox />
                 </div>
             </div>
+        )
+    }
+}
+
+class AnswersRadio extends React.Component {
+    renderButtons() {
+        const listItems = this.props.value.map((item) => {
+            return (
+                <li key={item}>
+                    <label className="container">{item}
+                        <input type="radio" name={item} />
+                        <span className="checkmark"></span>
+                    </label>
+                </li>
+            );
+        });
+        return listItems;
+    }
+
+    render() {
+        return (
+            <ul className="radio-list">
+                {this.renderButtons()}
+            </ul>
+        )
+    }
+}
+
+class AnswersCheckbox extends React.Component {
+    render() {
+        return null
+    }
+}
+
+class QuestionText extends React.Component {
+    render() {
+        return (
+            <h3>{this.props.value}</h3>
         )
     }
 }
