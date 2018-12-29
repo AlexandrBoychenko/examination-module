065d84248a967374a823cdbd596b098d147fd40b
Alexandr <alexandr.rogansky@gmail.com>
2018-12-29 16:32:51 +0200

    Separate classes for radio and checkbox input have been removed

--- examination-module/src/index.js
+++ examination-module/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './style/index.css';
 import './style/default.css';
 import './style/radio.css';
+import './style/checkboxes.css';
 
 class Examine extends React.Component {
 
@@ -56,73 +57,122 @@ class Questions extends React.Component {
             <div className="questions">
                 <div className="question">
                     <QuestionTitle
-                        value={'Вопрос 1'}
+                        value={1}
                     />
                     <QuestionText
                         value={'Сколько планет в солнечной системе?'}
                     />
                     <div className="question-body">
-                        <AnswersRadio
-                            value={[7, 5, 9, 8]}
+                        <AnswersInput
+                            value={{
+                                items: [7, 5, 9, 8],
+                                type: "radio"
+                            }}
                         />
                     </div>
                 </div>
                 <div className="question">
                     <QuestionTitle
-                        value={'Вопрос 2'}
+                        value={2}
                     />
                     <QuestionText
                         value={'Что характерно для кометы?'}
                     />
-                    <AnswersCheckbox />
+                    <div className="question-body">
+                        <AnswersInput
+                            value={{
+                                items: [
+                                    "Имеет хвост",
+                                    "Вращается вокруг солнца",
+                                    "Состоит из газа и пыли",
+                                    "Существует только во внутренней области солнечной системы",
+                                    "Не имеет ядра"
+                                ],
+                                type: "checkbox"
+                            }}
+                        />
+                    </div>
+                </div>
+                <div className="question">
+                    <QuestionTitle
+                        value={3}
+                    />
+                    <QuestionText
+                        value={'Что такое звезда?'}
+                    />
+                    <div className="question-body">
+                        <AnswersTexInput />
+                    </div>
                 </div>
             </div>
         )
     }
 }
 
-class AnswersRadio extends React.Component {
-    renderButtons() {
-        const listItems = this.props.value.map((item) => {
-            return (
-                <li key={item}>
-                    <label className="container">{item}
-                        <input type="radio" name="answer" value={item}/>
-                        <span className="checkmark"></span>
-                    </label>
-                </li>
-            );
+class AnswersInput extends React.Component {
+    renderItems() {
+        const listItems = this.props.value.items.map((item) => {
+            return this.renderByType(item)
         });
         return listItems;
     }
 
+    renderByType(item) {
+        switch(this.props.type) {
+            case 'radio':
+                this.className = 'radio-list';
+                return (
+                    <li key={item}>
+                        <label className="container">{item}
+                            <input type="radio" name="answer" value={item}/>
+                            <span className="checkmark"></span>
+                        </label>
+                    </li>
+                )
+            case 'checkbox':
+                this.className = 'checkbox-list';
+                return (
+                    <li key={item}>
+                        <label className="container container-checkbox">{item}
+                            <input type="checkbox" />
+                            <span className="checkmark-checkbox"></span>
+                        </label>
+                    </li>
+                )
+        }
+    }
+
     render() {
         return (
-            <ul className="radio-list">
-                {this.renderButtons()}
+            <ul className={this.className}>
+                {this.renderItems()}
             </ul>
         )
     }
 }
 
-class AnswersCheckbox extends React.Component {
+class AnswersTexInput extends React.Component {
     render() {
-        return null
-    }
+        return(
+            <textarea name="answer" id="" cols="30" rows="10" placeholder="Введите ваш ответ здесь"></textarea>
+        )
+    } 
 }
 
 class QuestionText extends React.Component {
     render() {
         return (
-            <h3>{this.props.value}</h3>
+            <h2>{this.props.value}</h2>
         )
     }
 }
 
-function QuestionTitle() {
-    return (
-        <h1>Это вопрос!</h1>
-    )
+class QuestionTitle extends React.Component {
+    render() {
+        return (
+            <h3 class="question-title">Вопрос {this.props.value}</h3>
+        )
+    }
 }
 
 ReactDOM.render(<Examine />, document.getElementById('root'));
