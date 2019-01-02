9595195e69f446830b982e92fa75a79bea994242
alexandr.rogansky@gmail.com <alex19wave>
2019-01-02 12:02:21 +0200

    Updates

---
+++ precache-manifest.43dad739d86f4b40e250ae64b36f225e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3672f08c47486c47f0cf",
+    "url": "/examination-module/static/js/runtime~main.3672f08c.js"
+  },
+  {
+    "revision": "1fa1b5ae3cd793653b8d",
+    "url": "/examination-module/static/js/main.1fa1b5ae.chunk.js"
+  },
+  {
+    "revision": "393e5c21aebdd32d3035",
+    "url": "/examination-module/static/js/1.393e5c21.chunk.js"
+  },
+  {
+    "revision": "1fa1b5ae3cd793653b8d",
+    "url": "/examination-module/static/css/main.9575ece9.chunk.css"
+  },
+  {
+    "revision": "6207a7df342142e444db7c14410bec68",
+    "url": "/examination-module/index.html"
+  }
+];
