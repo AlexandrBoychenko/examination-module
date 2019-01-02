c0a6864034b5974cf7716ddfdfd4e4d937e39faa
alexandr.rogansky@gmail.com <alex19wave>
2019-01-02 13:22:15 +0200

    Styles for modal window are moved from modal.js file to the common css styles. PropTypes usages are removed

--- src/modal.js
+++ src/modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import './style/modal.css'
 
 class Modal extends React.Component {
     render() {
@@ -8,37 +8,10 @@ class Modal extends React.Component {
             return null;
         }
 
-        // The gray background
-        const backdropStyle = {
-            position: 'fixed',
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            backgroundColor: 'rgba(0,0,0,0.3)',
-            padding: '10%'
-        };
-
-        // The modal "window"
-        const modalStyle = {
-            backgroundColor: '#fff',
-            borderRadius: 5,
-            maxWidth: 500,
-            margin: '0 auto',
-            padding: 30
-        };
-
-        const modalText = {
-            minHeight: '100px',
-            paddingTop: '5%'
-        };
-
         return (
-            <div className="backdrop" style={backdropStyle}>
-                <div className="modal" style={modalStyle}>
-                    {this.props.children}
-
-                    <div className="modalText" style={modalText}>
+            <div className="backdrop">
+                <div className="modal">
+                    <div className="modalText">
                         Каждый не отвеченный ответ считается неправильным, Вы уверены что хотите продолжить?
                     </div>
                     <div className="btn-footer">
@@ -54,12 +27,5 @@ class Modal extends React.Component {
         );
     }
 }
-
-Modal.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    onSubmit: PropTypes.func.isRequired,
-    show: PropTypes.bool,
-    children: PropTypes.node
-};
 
 export default Modal;
