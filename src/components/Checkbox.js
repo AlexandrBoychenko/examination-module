8809ed91ac7bce2e4b18a5c8cf740d575a91df1d
alexandr.rogansky@gmail.com <alex19wave>
2019-01-25 16:42:31 +0200

    handleChangeCheckbox method of Checkbox component has been refactored

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -16,24 +16,31 @@ class Checkbox extends React.Component {
     }
 
     handleChangeCheckbox(e) {
-        let currentValues = this.state.checked.slice();
-        let targetPos = currentValues.indexOf(e.target.value);
-        if (Object.keys(this.props.pastValues).length) {
-            if (~targetPos) {
-                currentValues.splice(targetPos, 1);
-            } else {
-                currentValues.push(e.target.value);
-            }
-        } else {
-            if (currentValues.length > 1) {
-                currentValues = [e.target.value];
-            } else {
-                currentValues.push(e.target.value)
-            }
-        }
-
-        this.setState({checked: currentValues});
+        let checkedElements = this.displayUserChoice(e.target.value);
+        this.setState({checked: checkedElements});
         this.props.context.onAnswerChangeCheckbox(e.target.value, this.props.id, 'Checkbox');
+    }
+
+    displayUserChoice(target) {
+        let currentValues = this.state.checked.slice();
+        let targetPos = currentValues.indexOf(target);
+        let isItHasPastValues = Object.keys(this.props.pastValues).length
+
+        if (isItHasPastValues) {
+            currentValues = this.handleCheckboxes(currentValues, target, (targetPos !== -1), targetPos);
+        } else {
+            currentValues = this.handleCheckboxes(currentValues, target, (currentValues.length > 1));
+        }
+        return currentValues
+    }
+
+    handleCheckboxes(currentValues, target, condition, targetPos) {
+        if (condition) {
+            Number.isInteger(targetPos) ? currentValues.splice(targetPos, 1) : currentValues = [target];
+        } else {
+            currentValues.push(target)
+        }
+        return currentValues
     }
 
     renderItems() {
